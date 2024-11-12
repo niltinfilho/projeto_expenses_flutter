9266d0c9b0e026827c8f84ab0ce7531f8b043e6c
Nilton Filho <niltonfilho115@gmail.com>
2024-11-12 00:38:03 -0300

    feat: estilizando o texto do preco

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,36 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: _transactions.map((tr) {
                 return Card(
-                  child: Text(tr.title),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          width: 2,
+                          color: Colors.purple,
+                        )),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tr.value.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(tr.title),
+                          Text(tr.date.toString()),
+                        ],
+                      )
+                    ],
+                  ),
                 );
               }).toList(),
             )

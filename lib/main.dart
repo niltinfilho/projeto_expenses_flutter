097c542baf67195a8348c9573c983bce719147af
Nilton Filho <niltonfilho115@gmail.com>
2024-11-12 00:54:05 -0300

    feat: instalando dependencias e melhorando a exibicao da data da compra

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_expenses_flutter/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -65,7 +66,7 @@ class MyHomePage extends StatelessWidget {
                         )),
                         padding: EdgeInsets.all(10),
                         child: Text(
-                          tr.value.toString(),
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
@@ -84,7 +85,7 @@ class MyHomePage extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date),
                             style: TextStyle(
                               color: Colors.grey,
                             ),

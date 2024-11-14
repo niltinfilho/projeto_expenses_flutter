b7af5f568ae406a92e7e15009bdbd84d3799d2c2
Nilton Filho <niltonfilho115@gmail.com>
2024-11-13 22:12:27 -0300

    feat: criando o botao de nova transacao

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyHomePage extends StatelessWidget {
           title: Text('Despesas Pessoais'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
@@ -96,6 +96,35 @@ class MyHomePage extends StatelessWidget {
                   ),
                 );
               }).toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Titulo:'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Valor (R\$):'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          style: ButtonStyle(
+                            foregroundColor:
+                                WidgetStateProperty.all<Color>(Colors.purple),
+                          ),
+                          child: Text('Nova Transacao'),
+                          onPressed: () {},
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ));

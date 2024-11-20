0247409adad0b5ecd606678139579d6a8315153a
Nilton Filho <niltonfilho115@gmail.com>
2024-11-19 23:13:31 -0300

    feat: tornando a tela scrollavel para nao interferir quando o teclado estiver aberto

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,53 +9,58 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tr) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
-                ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  width: 2,
-                  color: Colors.purple,
-                )),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'R\$ ${tr.value.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((tr) {
+            return Card(
+              child: Row(
                 children: [
-                  Text(
-                    tr.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      width: 2,
+                      color: Colors.purple,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
-                  Text(
-                    DateFormat('d MMM y').format(tr.date),
-                    style: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('d MMM y').format(tr.date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
